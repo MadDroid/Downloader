a345a9446ae52fd88093a5e074c9a12479d340eb
Mateus Becker <mateusbecker17@outlook.com>
2018-09-30 13:30:40 -0300

    Added cancel button and changed some logic

--- Downloader/MainWindow.xaml.cs
+++ Downloader/MainWindow.xaml.cs
@@ -11,6 +11,10 @@ namespace Downloader
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly WebClient client = new WebClient();
+
+        private string filePath;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -20,6 +24,47 @@ namespace Downloader
         {
             InputSavePath.Text = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
             InputURL.Text = "https://speed.hetzner.de/100MB.bin";
+            client.DownloadProgressChanged += (s, a) =>
+            {
+                var progress = (100 * a.BytesReceived) / a.TotalBytesToReceive;
+                Dispatcher.Invoke(() =>
+                {
+                    DownloadLabel.Content = $"Download Progress: {progress}%";
+                    progressDownload.Value = progress;
+                });
+            };
+
+            client.DownloadFileCompleted += (s, a) =>
+            {
+                progressDownload.Visibility = Visibility.Hidden;
+                DownloadLabel.Visibility = Visibility.Hidden;
+                DownloadButton.Content = "Download";
+                InputSavePath.IsEnabled = true;
+                InputURL.IsEnabled = true;
+                BrowseButton.IsEnabled = true;
+                if (a.Cancelled)
+                {
+                    if (File.Exists(filePath))
+                        File.Delete(filePath);
+                    return;
+                }
+                else if (a.Error is null)
+                {
+                    System.Windows.MessageBox.Show("Download completed", "Downloader", MessageBoxButton.OK);
+                }
+                else
+                {
+                    if (a.Error is WebException exception)
+                    {
+                        if (exception.Status == WebExceptionStatus.UnknownError)
+                        {
+                            System.Windows.MessageBox.Show("Não foi possível baixar o arquivo.\nPor favor tenha certeza que o arquivo que está tentando baixar exista.", "Downloader", MessageBoxButton.OK, MessageBoxImage.Error);
+                            return;
+                        }
+                    }
+                    System.Windows.MessageBox.Show(a.Error.Message, "Downloader", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            };
         }
 
         private void BrowseButton_Click(object sender, RoutedEventArgs e)
@@ -35,6 +80,12 @@ namespace Downloader
 
         private void DownloadButton_Click(object sender, RoutedEventArgs e)
         {
+            if ((string)DownloadButton.Content == "Cancel")
+            {
+                client.CancelAsync();
+                return;
+            }
+
             if (string.IsNullOrEmpty(InputSavePath.Text))
             {
                 System.Windows.MessageBox.Show("Please specify a valid save location", "Downloader", MessageBoxButton.OK, MessageBoxImage.Exclamation);
@@ -56,39 +107,18 @@ namespace Downloader
             if (Uri.TryCreate(InputURL.Text, UriKind.Absolute, out Uri uri))
             {
                 string fileName = Path.GetFileName(uri.AbsolutePath);
-                string filePath = Path.Combine(InputSavePath.Text, fileName);
+                filePath = Path.Combine(InputSavePath.Text, fileName);
+
+                DownloadLabel.Visibility = Visibility.Visible;
+                progressDownload.Visibility = Visibility.Visible;
+                InputSavePath.IsEnabled = false;
+                InputURL.IsEnabled = false;
+                BrowseButton.IsEnabled = false;
 
                 DownloadLabel.Content = "Starting download...";
 
-                using (var client = new WebClient())
-                {
-                    client.DownloadProgressChanged += (s, a) =>
-                    {
-                        var progress = (100 * a.BytesReceived) / a.TotalBytesToReceive;
-                        Dispatcher.Invoke(() =>
-                        {
-                            DownloadLabel.Content = $"Download Progress: {progress}%";
-                            progressDownload.Value = progress;
-                        });
-                    };
-
-                    client.DownloadFileCompleted += (s, a) =>
-                    {
-                        if (File.Exists(filePath))
-                        {
-                            System.Windows.MessageBox.Show("Download completed", "Downloader", MessageBoxButton.OK);
-                        }
-                    };
-
-                    try
-                    {
-                        client.DownloadFileAsync(uri, filePath);
-                    }
-                    catch (Exception ex)
-                    {
-                        System.Windows.MessageBox.Show(ex.Message, "Downloader", MessageBoxButton.OK, MessageBoxImage.Error);
-                    }
-                }
+                DownloadButton.Content = "Cancel";
+                client.DownloadFileTaskAsync(uri, filePath);
             }
             else
             {
@@ -96,6 +126,9 @@ namespace Downloader
             }
         }
 
-
+        private void Window_Unloaded(object sender, RoutedEventArgs e)
+        {
+            client.Dispose();
+        }
     }
 }
